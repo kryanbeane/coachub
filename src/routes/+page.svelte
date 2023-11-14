8ac38971bec8171959224759ecfb1c2d30ec1179
Bryan Keane <bryan.keane@genesys.com>
2023-11-14 18:55:08 +0000

    CHUB-7: Set up tailwind css and svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>I am the Coachub app</h1>
+<h1 class="text-3xl font-bold underline">I am the Coachub app</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+</style>
