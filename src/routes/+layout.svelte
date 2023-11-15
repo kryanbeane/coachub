2ff257550ef924d5bd4762916b10fb06d5af6b43
Bryan Keane <bryan.keane@genesys.com>
2023-11-15 14:42:42 +0000

    CHUB-7: Initialised skeleton ui repo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	import Sidebar from '../navigation/sidebar.svelte';
+	import Appbar from '../navigation/appbar.svelte';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<Appbar />
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Sidebar />
+	</svelte:fragment>
+
+	<!-- Page Content -->
+	
+	<slot />
+</AppShell>
